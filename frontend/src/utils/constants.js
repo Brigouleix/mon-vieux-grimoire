d25c4fc2647dd6a7c1ef1a1cd27db14cb8e31abc
Antoine <antoine.brigouleix@gmail.com>
2024-06-20 18:37:01 +0200

    url new

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://mon-vieux-grimoire-geaf-nok1li720-brigouleixs-projects.vercel.app';
+const API_URL = 'mongodb+srv://brigoo:Erwan1234*@atlascluster.d2l6kub.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
